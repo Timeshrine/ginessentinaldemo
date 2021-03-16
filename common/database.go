5afc9fcef136a399f1c467074272da7fd81fd55b
ethereal <whitefreesky@gmail.com>
2021-03-16 16:34:00 +0800

    0.0.2

---
+++ common/database.go
@@ -0,0 +1,33 @@
+package common
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() *gorm.DB {
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "1209801743"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(mysql.Open(args))
+	if err != nil {
+		panic("failed to connect database,err: " + err.Error())
+	}
+	return db
+}
+func GetDB() *gorm.DB {
+	return DB
+}
---
+++ controller/UserController.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	"oceanlearn.teach/ginessential/common"
+	"oceanlearn.teach/ginessential/modle"
+	"oceanlearn.teach/ginessential/util"
+)
+
+func Register(ctx *gin.Context) {
+	DB := common.GetDB()
+	//获取参数
+	name := ctx.PostForm("name")
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	//数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于六位"})
+		return
+	}
+	if len(name) == 0 {
+		name = util.RandsString(10)
+	}
+	log.Print(name, telephone, password)
+	//判断手机号是否存在
+	if isTelephoneExist(DB, telephone) {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
+		return
+	}
+
+	//创建用户
+	newUser := modle.User{
+		Name:      name,
+		Telephone: telephone,
+		Password:  password,
+	}
+	DB.Create(&newUser)
+
+	//返回结果
+	ctx.JSON(200, gin.H{
+		"msg": "注册成功",
+	})
+}
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user modle.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	db.AutoMigrate(&modle.User{})
+	return false
+}
